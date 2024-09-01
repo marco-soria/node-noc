fbf0e0c8d8e6a62167d40acbe3ba83fcb1c58fa3
marco-soria <marco.soriaa7@gmail.com>
2024-09-01 09:35:15 -0500

    noc init

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,9 +1,27 @@
+import { CronService } from "./cron/cron-service";
+import { CheckService } from "../domain/use-cases/checks/check-service";
 
 
 
 export class Server {
     public static start() {
         console.log('Server started');
+
+
+        CronService.createJob(
+            '*/5 * * * * *',
+            () => {
+                new CheckService(
+                    () => {
+                        console.log('Success');
+                    },
+                    (error) => {
+                        console.log('Error', error);
+                    }
+                ).execute('https://www.google.com'
+                )
+            }
+        )
     }
 
 }
