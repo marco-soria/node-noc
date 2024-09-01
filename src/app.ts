8423da6ede910cd93dc4026943c7e346930b38f3
marco-soria <marco.soriaa7@gmail.com>
2024-09-01 15:56:04 -0500

    repository pattern

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,22 @@
-
+//import { PrismaClient } from '@prisma/client';
+//import { envs } from './config/plugins/envs.plugin';
+//import { MongoDatabase } from './data/mongo';
 import { Server } from './presentation/server';
 
 
-
-
 (async() => {
-    main();
-  })();
-  
-  
-  async function main(){
-      Server.start();
-  
-  }
-  
+  main();
+})();
+
+
+async function main(){
+
+//   await MongoDatabase.connect({
+//     mongoUrl: envs.MONGO_URL,
+//     dbName: envs.MONGO_DB_NAME,
+//   });
+
+
+  Server.start();
+}
+
--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -1,39 +1,57 @@
-
+import { LogEntity, LogSeverityLevel } from '../../entities/log.entity';
+import { LogRepository } from '../../repository/log.repository';
 
 interface CheckServiceUseCase {
-    execute(url: string): Promise<boolean>;
+  execute( url: string ):Promise<boolean>;
 }
 
-type SuccessCallback = () => void
-type ErrorCallback = (error: string) => void
 
-// type SuccessCallback = (() => void) | undefined;
-// type ErrorCallback = (( error: string ) => void) | undefined;
+type SuccessCallback = (() => void) | undefined;
+type ErrorCallback = (( error: string ) => void) | undefined;
+
+
+
 
 export class CheckService implements CheckServiceUseCase {
-    constructor(
-        private readonly successCallback: SuccessCallback,
-        private readonly errorCallback: ErrorCallback
-    ) {}
+
+  constructor(
+    private readonly logRepository: LogRepository,
+    private readonly successCallback: SuccessCallback,
+    private readonly errorCallback: ErrorCallback
+  ) {}
 
 
-    public async execute(url: string): Promise<boolean> {
+  public async execute( url: string ): Promise<boolean> {
 
-        try {
-            const req = await fetch(url);
-            if (!req.ok) {
-                throw new Error(`Error on check service ${url}`);
-            }
-            console.log(`${url} is up`);
-            this.successCallback?.();
-            return true;
-        } catch (error) {
-            console.log(`${error}`);
+    try {
+      const req = await fetch( url );
+      if ( !req.ok ) {
+        throw new Error( `Error on check service ${ url }` );
+      }
 
-            this.errorCallback(`${error}`);
-            return false;
-        }
+      const log = new LogEntity({
+        message: `Service ${ url } working`, 
+        level: LogSeverityLevel.low,
+        origin: 'check-service.ts'
+      });
+      this.logRepository.saveLog( log );
+      this.successCallback && this.successCallback();
 
-        
+      return true;
+    } catch (error) {
+      const errorMessage = `${url} is not ok. ${ error }`;
+      const log = new LogEntity({ 
+        message:errorMessage , 
+        level: LogSeverityLevel.high,
+        origin: 'check-service.ts'
+       });
+      this.logRepository.saveLog(log);
+      
+      this.errorCallback && this.errorCallback( errorMessage );
+
+      return false;
     }
+
+  }
+
 }
